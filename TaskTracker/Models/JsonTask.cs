ee4da92870f27d10b49325da29067ee92712576f
Patryk <patsenn2000@gmail.com>
2024-12-28 11:50:50 +0000

    Add task structure

---
+++ TaskTracker/Models/JsonTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace TaskTracker.Models
+{
+    public class JsonTask
+    {
+        public enum TaskStatus
+        {
+            todo, inProgress, done
+        };
+
+
+        public int Id;
+        public string Description;
+        public TaskStatus Status;
+        public DateTime CreatedAt;
+        public DateTime UpdatedAt;
+
+
+        public JsonTask(int id, string description)
+        {
+            Id = id;
+            Description = description;
+            Status = TaskStatus.todo;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
+
